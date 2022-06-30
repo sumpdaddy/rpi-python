05dd1d62312d2dc288dc1b489acb2267c736552d
sumpdaddy <108490618+sumpdaddy@users.noreply.github.com>
2022-06-30 18:45:48 -0400

    Update tof-measure.py

--- tof-measure.py
+++ tof-measure.py
@@ -1,48 +1,50 @@
-# SPDX-FileCopyrightText: 2021 Smankusors for Adafruit Industries
-# SPDX-License-Identifier: MIT
+# Sump monitoring measurement call. Meant to run once per measurement.
 
-# Simple demo of the VL53L0X distance sensor with continuous mode.
-# Will print the sensed range/distance as fast as possible.
 import time
-
 import board
 import busio
-
 import adafruit_vl53l0x
+
+# Set up some functional details
+
+
+pitbottom=600   # the approximate bottom of the pit (in mm)
+meas_file="/etc/sumpdaddy"
 
 # Initialize I2C bus and sensor.
 i2c = busio.I2C(board.SCL, board.SDA)
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
-# Optionally adjust the measurement timing budget to change speed and accuracy.
-# See the example here for more details:
-#   https://github.com/pololu/vl53l0x-arduino/blob/master/examples/Single/Single.ino
-# For example a higher speed but less accurate timing budget of 20ms:
-# vl53.measurement_timing_budget = 20000
-# Or a slower but more accurate timing budget of 200ms:
+# set timing budget to 300ms (we are not in a hurry)
 vl53.measurement_timing_budget = 300000
-# The default timing budget is 33ms, a good compromise of speed and accuracy.
 
-# You will see the benefit of continous mode if you set the measurement timing
-# budget very high, while your program doing something else. When your program done
-# with something else, and the sensor already calculated the distance, the result
-# will return instantly, instead of waiting the sensor measuring first.
-
-# Main loop will read the range and print it every second.
+# Main loop will read the range in continuous mode, 1/sec
+# after 40 samples it will average all samples and save result to file
 
 with vl53.continuous_mode():
+
     readingsum = 0
+    #  initialize the last good reading at 100mm
     oldreading = 100
     newreading = 100
+
+    # loop for 40 seconds
     for x in range(1,40):
-        newreading = round(500-vl53.range,2)
-        if newreading < 25 or newreading > 325:
+        # take the reading and subtract it from the bottom to get the height
+        newreading = round(pitbottom-vl53.range,2)
+        # check the reading is realistic, if not then keep last good reading
+        if newreading < 25 or newreading > 425:
             newreading = oldreading
+        # generate the average
         readingsum = readingsum + newreading
         oldreading = newreading
         time.sleep(1)
+
+    # calculate the reading and save it to the file
     movingavg = str(readingsum/40)
-    f = open("/etc/sumpdaddy", "w")
+    f = open(meas_file, "w")
     f.write(movingavg)
     f.close()
+
+# stop the reading since we are about to close
 vl53.stop_continuous()
