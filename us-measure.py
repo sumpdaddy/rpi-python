4dad829e4295c57d260dbde0def084ff0cad42a2
sumpdaddy <108490618+sumpdaddy@users.noreply.github.com>
2022-07-01 08:08:45 -0400

    normalized names
    Put both sets of scripts into normal namespace and cleaned both up internally. Then added cacti graph exports.

--- us-measure.py
+++ us-measure.py
@@ -16,6 +16,12 @@ GPIO_ECHO = 24
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
+# set up some details
+
+runtime=50   # how many samples to get (approx seconds to run)
+pitbottom=60 # approximate bottom of pit in cm
+meas_file="/etc/sumpdaddy"
+
 def distance():
 
     # set Trigger to HIGH
@@ -37,7 +43,14 @@ def distance():
         StopTime = time.time()
 
     # time difference between start and arrival
+    # using an IEE754 float as a time DOES impact results since the data we need
+    # is in the microsecond range, SO
+    # each reading is forced to a decimal data type to preserve precision
+    # everything to the left of the decimal is thrown away to preserve precision
     TimeElapsed = (Decimal(str(StopTime)) % 1) - (Decimal(str(StartTime)) % 1)
+
+
+    # if the reading spanned more than 1 second, calc will be negative. so, fix that
     if TimeElapsed < 0:
         TimeElapsed = TimeElapsed + 1
 
@@ -53,11 +66,11 @@ if __name__ == '__main__':
     readingsum = 0
 
 
-    for x in range(1,55):
-        readingsum = readingsum + round(50-distance(),2)
+    for x in range(1,runtime):
+        readingsum = readingsum + round(pitbottom-distance(),2)
         time.sleep(.95)
-    movingavg = str(readingsum/55)
-    f = open("/etc/sumpdaddy", "w")
+    movingavg = str(readingsum/runtime)
+    f = open(meas_file, "w")
     f.write(movingavg)
     f.close()
     GPIO.cleanup()
